f99be123048cad9ac1a98e4e58e1802e38d09d7a
marcaopxt <marcuspxt@gmail.com>
2023-02-25 17:48:56 -0300

    fix(aks): variables

---
+++ releases/aks-free-tier/variables.f
@@ -0,0 +1,19 @@
+variable "prefix" {
+  description = "A prefix used for all resources in this example"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be provisioned"
+}
+variable "tenant_id" {
+  type = string
+  description = ""
+}
+variable "subscription_id" {
+  type = string
+  description = ""
+}
+variable "cluster_name" {
+  type = string
+  description = ""
+}
