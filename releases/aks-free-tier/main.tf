3c081e11ff1fcb56a2fe56a5c7608f9cffba175f
marcaopxt <marcuspxt@gmail.com>
2023-02-25 17:29:46 -0300

    fix(provider): wrong quotes

--- releases/aks-free-tier/main.tf
+++ releases/aks-free-tier/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
-    "azurerm" = {
-      source = "hashicorp/azurerm”"
+    azurerm = {
+      source = "hashicorp/azurerm"
       version = ">= 2.56.0"
     }
   }
