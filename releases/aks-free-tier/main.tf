2ef6f1a2ef1680842d75fe3aa4c210f88b180945
Marcus Peixoto <marcuspxt@gmail.com>
2023-02-26 13:27:56 -0300

    fix(azure): credentials vars

--- releases/aks-free-tier/main.tf
+++ releases/aks-free-tier/main.tf
@@ -35,17 +35,7 @@ resource "azurerm_kubernetes_cluster" "aks_free_tier_demo" {
   }
 
   tags = {
-    Environment = "Production"
+    Environment = "Staging"
   }
 }
 
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.aks_free_tier_demo.kube_config.0.client_certificate
-  sensitive = true
-}
-
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.aks_free_tier_demo.kube_config_raw
-
-  sensitive = true
-}
